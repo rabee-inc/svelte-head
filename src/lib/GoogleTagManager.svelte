3767c74bbe2a051d791b5a55bd8b38fe336f7b75
phi <phi1618jp@gmail.com>
2022-02-15 03:30:06 +0900

    fix GoogleTagManager.svelte

---
+++ src/lib/GoogleTagManager.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let id = '';
+</script>
+
+<svelte:head>
+  {#if id}
+    {@html `
+    <script>
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','${id}');
+    </script>
+    `}
+  {/if}
+</svelte:head>
--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,4 @@ export {setOptions, options} from './options';
 
 // components
 export { default as Meta } from './Meta.svelte';
+export { default as GoogleTagManager } from './GoogleTagManager.svelte';
